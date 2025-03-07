85496f9a660da57cb3b37e1c3a0cbf7aba44ff41
Musraf khan <kmusharraf07878@gmail.com>
2025-03-07 22:10:45 +0530

    Delete CY033 directory

--- CY033/application.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from tkinter import ttk
-
-class ApplicationTab(ttk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
-        label = ttk.Label(self, text="Application Management")
-        label.pack(pady=20)
